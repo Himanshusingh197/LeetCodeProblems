e1892fce179854d05bef480f6bc90c7e9dd95b79
Himanshu Rajput <121163531+Himanshusingh197@users.noreply.github.com>
2026-08-11 18:21:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (50.00%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+
+        if(n != m){
+            return false;
+        }
+        unordered_map<char, int> mp;
+
+        for(int i=0; i<n; i++){
+            mp[s[i]]++;
+        }
+
+        for(int i=0; i<m; i++){
+            mp[t[i]]--;
+        }
+
+        for(auto x : mp){
+            if(x.second != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
