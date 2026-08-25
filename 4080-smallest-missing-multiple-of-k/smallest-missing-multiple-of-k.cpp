49096dc60f4bbaf831f41bfc0f863554216ef215
Himanshu Rajput <121163531+Himanshusingh197@users.noreply.github.com>
2026-08-25 13:57:11 +0530

    Time: 3 ms (38.06%) | Memory: 23.9 MB (94.59%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -3,17 +3,13 @@ public:
     int missingMultiple(vector<int>& nums, int k) {
         int n = nums.size();
 
-        unordered_map<int, int>mp;
+        int multiple = k;
+        sort(nums.begin(), nums.end());
         for(int i=0; i<n; i++){
-            mp[nums[i]]++;
-        }
-
-        for(int i=1 ; ; i++){
-            int multiple = i * k;
-
-            if(mp.find(multiple) == mp.end()){
-                return multiple;
+            if(nums[i] == multiple){
+                multiple += k;
             }
         }
+        return multiple;
     }
 };
