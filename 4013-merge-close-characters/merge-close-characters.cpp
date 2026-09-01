2f15ea1c5baa6b2f12cc0ee5e17e30d4682eeb89
Himanshu Rajput <121163531+Himanshusingh197@users.noreply.github.com>
2026-09-01 16:45:29 +0530

    Time: 7 ms (15.27%) | Memory: 10.8 MB (8.38%) - LeetSync

---
+++ 4013-merge-close-characters/merge-close-characters.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string mergeCharacters(string s, int k) {
+
+        while (true) {
+
+            bool merged = false;
+
+            for (int i = 0; i < s.size(); i++) {
+
+                for (int j = i + 1; j < s.size(); j++) {
+
+                    if (s[i] == s[j] && j - i <= k) {
+                        
+                        s = s.substr(0, j) + s.substr(j + 1);
+
+                        merged = true;
+                        break;
+                    }
+                }
+
+                if (merged)
+                    break;
+            }
+
+            if (!merged)
+                break;
+        }
+
+        return s;
+    }
+};
