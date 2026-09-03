724106f3cb3680cb32f1d4caa2c54c8e10b386ad
Himanshu Rajput <121163531+Himanshusingh197@users.noreply.github.com>
2026-09-03 16:46:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (72.42%) - LeetSync

---
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string toHex(int num) {
+
+        if(num == 0){
+            return "0";
+        }
+        unsigned int n = num;
+        string hex = "0123456789abcdef";
+        string ans = "";
+
+        while(n > 0){
+            int rem = n % 16;
+            ans += hex[rem];
+            n /= 16;
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
