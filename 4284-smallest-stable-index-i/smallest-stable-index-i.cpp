660f0185cf1cd58a393d7da44eac8b461c388003
Himanshu Rajput <121163531+Himanshusingh197@users.noreply.github.com>
2026-09-05 00:07:44 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (56.73%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -3,19 +3,18 @@ public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
 
-        for(int i = 0; i < n; i++) {
-            int maxEl = INT_MIN;
-            int minEl = INT_MAX;
+        vector<int> minFromIdx(n);
+        int minEl = INT_MAX;
+        for(int i = n-1; i >= 0; i--) {
+            minEl = min(minEl, nums[i]);
+            minFromIdx[i] = minEl;
+        }
 
-            for(int j = 0; j <= i; j++) {
-                maxEl = max(maxEl, nums[j]);
-            }
+        int maxEl = INT_MIN;
+        for(int i=0; i<n; i++){
+            maxEl = max(maxEl, nums[i]);
 
-            for(int j = i; j < n; j++) {
-                minEl = min(minEl, nums[j]);
-            }
-
-            if(maxEl - minEl <= k) {
+            if(maxEl - minFromIdx[i] <= k){
                 return i;
             }
         }
