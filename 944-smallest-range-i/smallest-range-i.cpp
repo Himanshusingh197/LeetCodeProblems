7f1b554942f56f0b83ad07a16f2d6e142df0537d
Himanshu Rajput <121163531+Himanshusingh197@users.noreply.github.com>
2026-09-05 16:40:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (25.00%) - LeetSync

--- 944-smallest-range-i/smallest-range-i.cpp
+++ 944-smallest-range-i/smallest-range-i.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
     int smallestRangeI(vector<int>& nums, int k) {
-        int minEl = nums[0];
-        int maxEl = nums[0];
+        int maxEl = *max_element(nums.begin(), nums.end());
+        int minEl = *min_element(nums.begin(), nums.end());
 
-        for(int i = 1; i < nums.size(); i++) {
-            minEl = min(minEl, nums[i]);
-            maxEl = max(maxEl, nums[i]);
-        }
-
-        return max(0, maxEl - minEl - 2 * k);
+        return max(0, maxEl - minEl - 2*k);
     }
 };
