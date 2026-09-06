69893f7e6092b2f9e10c4a985c0074952f9b20f3
Himanshu Rajput <121163531+Himanshusingh197@users.noreply.github.com>
2026-09-07 00:10:41 +0530

    Time: 2758 ms (5.03%) | Memory: 418.2 MB (7.80%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
+
+    typedef unsigned long long ull;
+    ull dp[1001][1001];
+    ull solve(string s, string t, int m, int n){
+        if(n == 0){
+            return dp[m][n] = 1;
+        }
+
+        if(m == 0){
+            dp[m][n] = 0;
+        }
+
+        if(dp[m][n] != -1){
+            return dp[m][n];
+        }
+
+        if(s[m-1] == t[n-1]){
+            return dp[m][n] = solve(s, t, m-1, n-1) + solve(s, t, m-1, n);
+        }else{
+            return dp[m][n] = solve(s, t, m-1, n);
+        }
+
+        return -1;
+    }
+
     int numDistinct(string s, string t) {
-        int n = s.size();
-        int m = t.size();
+        int m = s.size();
+        int n = t.size();
+        memset(dp, -1, sizeof(dp));
 
-        vector<vector<unsigned long long>> dp(
-            n + 1, vector<unsigned long long>(m + 1, 0)
-        );
-
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 1;
-        }
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                dp[i][j] = dp[i - 1][j];
-
-                if (s[i - 1] == t[j - 1]) {
-                    dp[i][j] += dp[i - 1][j - 1];
-                }
-            }
-        }
-
-        return (int)dp[n][m];
+        return solve(s, t, m, n);
     }
 };
