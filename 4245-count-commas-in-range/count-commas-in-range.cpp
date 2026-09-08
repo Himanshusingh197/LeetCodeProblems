4b40b5dafb0e30a66cd8228f30c91bedc2f34d2a
Himanshu Rajput <121163531+Himanshusingh197@users.noreply.github.com>
2026-09-08 12:21:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (84.49%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -6,11 +6,8 @@ public:
             return 0;
         }
 
-        int count = 0;
-        for(int i=1000; i<=n; i++){
-            count++;
-        }
+        int ans = (n - 1000) + 1;
 
-        return count;
+        return ans;
     }
 };
