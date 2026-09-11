29775189a5686ac49bfe76f9e0128933fecb78f5
Himanshu Rajput <121163531+Himanshusingh197@users.noreply.github.com>
2026-09-11 12:42:18 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (79.72%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -1,27 +1,41 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        set<int> st;
-        int n = digits.size();
+        vector<int> result;
+        vector<int> mp(10, 0);
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                for(int k=0; k<n; k++){
-                    if(i == j || j == k || k == i){
-                        continue;
-                    }
-
-                    int num = digits[i]*100 + digits[j]*10 + digits[k];
-                    if(num >= 100 && num%2 == 0){
-                        st.insert(num);
-                    }
-                }
-            }
+        for(int digit : digits){
+            mp[digit]++;
         }
-       int count = 0;
-       for(auto x : st){
-        count++;
-       }
+
+        for(int i=1; i<=9; i++){
+            if(mp[i] == 0)
+                continue;
+            mp[i]--;
+
+            for(int j=0; j<=9; j++){
+                if(mp[j] == 0)
+                    continue;
+                mp[j]--;
+
+                for(int k=0; k<=8; k += 2){
+                    if(mp[k] == 0)
+                        continue;
+                    mp[k]--;
+
+                    int  num = i*100 + j*10 +  k;
+                    result.push_back(num);
+                    mp[k]++; 
+                }
+                mp[j]++;
+            }
+            mp[i]++;
+        }
+
+        int count = 0;
+        for(auto it : result){
+            count++;
+        }
 
        return count;
     }
